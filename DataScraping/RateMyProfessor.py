8a8311205fb0004bab0b6efc43dc304ba4db2dd7
Serena Osuagwu <serenityosuagwu@gmail.com>
2025-03-01 22:40:32 -0500

    push code

--- DataScraping/RateMyProfessor.py
+++ DataScraping/RateMyProfessor.py
@@ -1,5 +1,6 @@
 import ratemyprofessor
 import json
+import Professor
 from datetime import datetime
 
 class DateTimeEncoder(json.JSONEncoder):
@@ -10,11 +11,35 @@ class DateTimeEncoder(json.JSONEncoder):
     
 school = ratemyprofessor.get_school_by_name("Drexel University")
 
-def getRatings(name):
-    professor = ratemyprofessor.get_professor_by_school_and_name(school, "Dimitrios Papadopoulos")
+def GetProfessors(profNames):
+    list_of_professors = []
+    list_of_inputProfessors = set()
+    
+    for profname in profNames:
+        professor = ratemyprofessor.get_professor_by_school_and_name(school, profName)
+        if professor:
+            list_of_inputProfessors.add(professor)
+
+    for currentProfessor in list_of_inputProfessors:
+        profid = currentProfessor.id
+        profName = currentProfessor.name
+        overallRating = currentProfessor.rating
+        difficulty = currentProfessor.difficulty
+        rmp = ratemyprofessor.professor.Professor.get_ratings(currentProfessor.name)
+        ratingMetadata = None
+        if (rmp is not None):
+            ratingMetadata = json.dumps([rating.__dict__ for rating in rmp], cls=DateTimeEncoder, indent=4)
+        
+        newProf = Professor.Professor(profid=profid, profName=profName, overallRating=overallRating, difficulty=difficulty, metaData=ratingMetadata)
+        list_of_professors.append(newProf)
+    
+    json_data = json.dumps([eachProfessor.__dict__ for eachProfessor in list_of_professors], indent=4)
+    return json_data
+
+    '''professor = ratemyprofessor.get_professor_by_school_and_name(school, "Dimitrios Papadopoulos")
 
     rmp = ratemyprofessor.professor.Professor.get_ratings(professor)
-
+    
     if professor:
         print(f"Name: {professor.name}")
         print(f"Rating: {professor.rating}")
@@ -30,4 +55,4 @@ def getRatings(name):
         print("Professor not found.")
 
     json_data = json.dumps([rating.__dict__ for rating in rmp], cls=DateTimeEncoder, indent=4)
-    print(json_data)
+    print(json_data)'''
--- DataScraping/TermMaster.py
+++ DataScraping/TermMaster.py
@@ -2,34 +2,75 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import json
 import time
+import Class
 
-def extractClasses(table):
-    # Extract table headers
-    headers = []
-    header_row = table.find_element(By.TAG_NAME, "thead")
-    header_cells = header_row.find_elements(By.TAG_NAME, "th")
-    for header in header_cells:
-        headers.append(header.text.strip())
+class TermMaster:
+    def __init__(self, importTable):
+        self.table = importTable
+        self.coursesNumber = None
+        self.professors = None
 
-    # Extract table rows
-    rows = []
-    table_body = table.find_element(By.TAG_NAME, "tbody")
-    table_rows = table_body.find_elements(By.TAG_NAME, "tr")
-
-    for row in table_rows:
-        cells = row.find_elements(By.TAG_NAME, "td")
-        if cells:
-            row_data = [cell.text.strip() for cell in cells]
-            if len(row_data) > 5:
-                rows.append(row_data)
-    return rows
-
-# Print or process the data
-    '''
-    print("Headers:", headers)
-    for row in rows:
-        print(row)
-        '''
     
 
+    def ExtractClasses(self):
+        # Extract table headers
+        headers = []
+        header_row = self.table.find_element(By.TAG_NAME, "thead")
+        header_cells = header_row.find_elements(By.TAG_NAME, "th")
+        for header in header_cells:
+            headers.append(header.text.strip())
+
+        # Extract table rows
+        rows = []
+        table_body = self.table.find_element(By.TAG_NAME, "tbody")
+        table_rows = table_body.find_elements(By.TAG_NAME, "tr")
+
+        for row in table_rows:
+            cells = row.find_elements(By.TAG_NAME, "td")
+            if cells:
+                row_data = [cell.text.strip() for cell in cells]
+                if len(row_data) > 5:
+                    rows.append(row_data)
+        self.SetClassesModel(rows)
+        self.ExtractProfessors(rows)
+
+    def SetClassesModel(self, classes):
+        list_of_classes = []
+        list_of_courseNumber = set()
+        for current in classes:
+            subjectCode = current[0] + " " + current[1]
+            list_of_courseNumber.add(subjectCode)
+            classType = current[2]
+            method = current[3]
+            section = current[4]
+            crn = current[5]
+            name = current[6]
+            splitDate = current[7].split(" ", 1)
+            
+            if (len(splitDate) > 1):
+                date = splitDate[0]
+                timesplit = splitDate[1].split('\n')
+                classTime = timesplit[0]
+            else :
+                date = splitDate[0]
+                classTime = splitDate[0]
+            professor = current[8]
+            newClass = Class.Class(subjectCode=subjectCode, classType=classType, method=method, section=section, crn=crn, name=name, date=date, classTime=classTime, professor=professor)
+            list_of_classes.append(newClass)
+
+        self.coursesNumber = list_of_courseNumber
+        json_data = json.dumps([eachClass.__dict__ for eachClass in list_of_classes], indent=4)
+        return json_data
+
+    def ExtractProfessors(self, rows):
+        list_of_professors = set()
+        for currentClass in rows:
+            possibleProfessor = currentClass[8]
+            if (possibleProfessor != None or possibleProfessor.lower() != "tbd"):
+                list_of_professors.add(possibleProfessor)
+        
+        self.professors = list_of_professors
+        
+    
