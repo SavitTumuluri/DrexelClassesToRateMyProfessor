35a5ba1623811291434db2e33e6909912364f57a
riaagarwal31 <ra926@drexel.edu>
2025-03-02 09:58:32 -0500

    Updates

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from transcriptParser import parse_transcipts
 app = Flask(__name__)
 
 #classes is an array of dictionary (str:str) one dict = one class
-classes = [{"course_code": "CS150", 
+""" classes = [{"course_code": "CS150", 
            "ClassTime": "09:00 am - 10:50 am", 
            "WeekDay": "F",
            "Professor": "Daniel W Moix", 
@@ -15,7 +15,9 @@ classes = [{"course_code": "CS150",
         "WeekDay": "M/W/F",
         "Professor": "Dr. John Doe",
         "ClassType": "Lecture"
-    }]
+    }] """
+
+classes = []
 
 @app.route('/', methods=['GET', 'POST'])
 
