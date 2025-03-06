a2f2743ee5fcba0950ef4858c76b8173c0119940
Condufunder <mynameissavit@gmail.com>
2025-03-05 23:26:02 -0500

    delete more sneaky stuff

--- DataScraping/Authenticator.py
+++ DataScraping/Authenticator.py
@@ -3,12 +3,12 @@ from pyzbar.pyzbar import decode
 import pyotp
 
 # Load the QR code image (screenshot it and save it first)
-'''image = cv2.imread("image.png")
+image = cv2.imread("authimage.png")
 
 # Decode QR code
 decoded_objects = decode(image)
 for obj in decoded_objects:
-    print("Decoded Data:", obj.data.decode("utf-8"))'''
+    print("Decoded Data:", obj.data.decode("utf-8"))
 
 secret = "7mrx5y2nvvvbrrgk"  # Use your extracted secret key
 totp = pyotp.TOTP(secret)
